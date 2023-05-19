7c2065ab7587bf0c70fa63cc2b88a5e667b03378
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2023-05-19 14:28:50 -0700

    manifesto

--- custom_product_screen/__manifest__.py
+++ custom_product_screen/__manifest__.py
@@ -1,19 +1,19 @@
 {
-            'name': 'Coffe shop',
-            'version': '16.0.0.9',
-                    'category': '',
-                    'description': 'Sobre escribe estilos y funcionalidad de componentes del punto de venta y agrega soporte para multiples pantallas (lectura-escritura y solo lectura)',
-                    'author': 'Quadro Soluciones',
-                    'images': [],
-                    'depends': ['point_of_sale','pos_enterprise'],
-                    'installable': True,
-                    'assets': {
-                        'web.assets_backend': [
-                                'custom_product_screen/static/src/**/*.js',
-                                'custom_product_screen/static/src/**/**/*.js',
-                        ],
-                        'web.assets_qweb': [
-                                'custom_product_screen/static/src/**/*.xml',
-                        ]
-                    }
+    'name': 'Coffe shop',
+    'version': '16.0.0.9',
+    'category': '',
+    'description': 'Sobre escribe estilos y funcionalidad de componentes del punto de venta y agrega soporte para multiples pantallas (lectura-escritura y solo lectura)',
+    'author': 'Quadro Soluciones',
+    'images': [],
+    'depends': ['point_of_sale', 'pos_enterprise'],
+    'installable': True,
+    'assets': {
+        'web.assets_backend': [
+            'custom_product_screen/static/src/**/*.js',
+            'custom_product_screen/static/src/**/*.js',
+        ],
+        'web.assets_qweb': [
+            'custom_product_screen/static/src/**/*.xml',
+        ]
+    }
 }
