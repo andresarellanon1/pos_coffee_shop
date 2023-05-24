ca6b54e6ac3b2524653aae71475423331cab13f5
jesus <pablo@quadrosoluciones.mx>
2023-05-24 16:07:21 -0700

    rewrite constructor product screen

--- custom_product_screen/static/src/js/ProductScreen.js
+++ custom_product_screen/static/src/js/ProductScreen.js
@@ -6,41 +6,40 @@ import NumberBuffer from 'point_of_sale.NumberBuffer'
 import { useExternalListener, onMounted, useState } from '@odoo/owl';
 
 patch(ProductScreen.prototype, "prototype patch", {
-    setup(){
-        useExternalListener(window,'click-product', this._clickProduct);
-         onRendered(() => {
-                if (this.env.isDebug()) {
-                    console.log('Rendered:', this.constructor.name);
-                }
-            });
- 
-            useListener('update-selected-orderline', this._updateSelectedOrderline);
-            useListener('select-line', this._selectLine);
-            useListener('set-numpad-mode', this._setNumpadMode);
-            useListener('click-product', this._clickProduct);
-            useListener('click-partner', this.onClickPartner);
-            useListener('click-pay', this._onClickPay);
-            useBarcodeReader({
-                product: this._barcodeProductAction,
-                weight: this._barcodeProductAction,
-                price: this._barcodeProductAction,
-                client: this._barcodePartnerAction,
-                discount: this._barcodeDiscountAction,
-                error: this._barcodeErrorAction,
-            });
-            NumberBuffer.use({
-                nonKeyboardInputEvent: 'numpad-click-input',
-                triggerAtInput: 'update-selected-orderline',
-                useWithBarcode: true,
-            });
-            onMounted(this.onMounted);
-            // Call `reset` when the `onMounted` callback in `NumberBuffer.use` is done.
-            // We don't do this in the `mounted` lifecycle method because it is called before
-            // the callbacks in `onMounted` hook.
-            onMounted(() => NumberBuffer.reset());
-            this.state = useState({
-                mobile_pane: this.props.mobile_pane || 'right',
-            });
+    setup() {
+        onRendered(() => {
+            if (this.env.isDebug()) {
+                console.log('Rendered:', this.constructor.name);
+            }
+        });
+        useExternalListener(window, 'click-product', this._clickProduct);
+        useExternalListener(window, 'update-selected-orderline', this._updateSelectedOrderline);
+        useExternalListener(window, 'select-line', this._selectLine);
+        useExternalListener(window, 'set-numpad-mode', this._setNumpadMode);
+        useExternalListener(window, 'click-product', this._clickProduct);
+        useExternalListener(window, 'click-partner', this.onClickPartner);
+        useExternalListener(window, 'click-pay', this._onClickPay);
+        useBarcodeReader({
+            product: this._barcodeProductAction,
+            weight: this._barcodeProductAction,
+            price: this._barcodeProductAction,
+            client: this._barcodePartnerAction,
+            discount: this._barcodeDiscountAction,
+            error: this._barcodeErrorAction,
+        });
+        NumberBuffer.use({
+            nonKeyboardInputEvent: 'numpad-click-input',
+            triggerAtInput: 'update-selected-orderline',
+            useWithBarcode: true,
+        });
+        onMounted(this.onMounted);
+        // Call `reset` when the `onMounted` callback in `NumberBuffer.use` is done.
+        // We don't do this in the `mounted` lifecycle method because it is called before
+        // the callbacks in `onMounted` hook.
+        onMounted(() => NumberBuffer.reset());
+        this.state = useState({
+            mobile_pane: this.props.mobile_pane || 'right',
+        });
     },
 });
 
