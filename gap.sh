390715b01fa31c15a64128ea433eef7bbc4d69bd
Jes Ale <jes@jes.ale>
2023-07-11 22:08:22 -0700

    commit automatico test2

--- gap.sh
+++ gap.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # TODO: add this as a plugin from neovim
 git add .
-git commit -m "scripted commit"
-echo $GITP
-echo $GITU
+git commit -m "$1"
 git push https://$GITU:$GITP@github.com/andresarellanon1/pos_coffee_shop.git
