cca8e5b91d97075b38015589d7a5462b0c96504a
Jes Ale <jes@jes.ale>
2023-07-11 22:02:52 -0700

    scripted commit

--- gap.sh
+++ gap.sh
@@ -2,4 +2,6 @@
 # TODO: add this as a plugin from neovim
 git add .
 git commit -m "scripted commit"
-git push https://${GITU}:${GITP}@github.com/andresarellanon1/pos_coffee_shop.git
+echo $GITP
+echo $GITU
+git push https://$GITU:$GITP@github.com/andresarellanon1/pos_coffee_shop.git
