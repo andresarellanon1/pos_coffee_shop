3aa9e7b8621b1f1c052c554260092efb65e3a9b5
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-13 12:35:04 -0700

    a

--- q_endpoint_catalog/__manifest__.py
+++ q_endpoint_catalog/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'REST Endpoint Catalog',
-    'version': '16.0.0.1',
+    'version': '16.0.0.2',
     'category': 'Extra Tools',
     'description': 'Manages a catalog of REST endpoints. Define URL, Method, Body, Response, Headers and hardcode Authorization (or not). Call from python functions or modules on demand. ',
     'author': 'Quadro Soluciones',
@@ -10,9 +10,14 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+        'views/endpoint_views.xml',
     ],
     'assets': {
         'point_of_sale.assets': [
+            # 'q_endpoint_catalog/static/src/xml/*.xml',
+            # 'q_endpoint_catalog/static/src/js/*.js',
+            # 'q_endpoint_catalog/static/src/css/dist/*.css',  # tailwind generated
+            # 'q_endpoint_catalog/static/src/css/*.css',
         ]
     }
 }
