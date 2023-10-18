18b14f5bb1ab56b1b833026761cbea0eb525b621
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-18 11:20:26 -0700

    contacts

--- tms_customer_waybill/wizards/tms_customer_waybill.py
+++ tms_customer_waybill/wizards/tms_customer_waybill.py
@@ -5,7 +5,7 @@ class CustomWizard(models.TransientModel):
     _name = 'tms_customer_waybill.customer_waybill_wizard'
     _description = 'Customer Waybill Wizard'
 
-    contact_id = fields.Many2one('res.partner', 'Contact', help='Select a contact from the Contacts module.', required=True)
-    endpoint_id = fields.Many2one('q_endpoint_catalog.q_endpoint', 'Endpoint',
-                                  domain="[('contact_id', '=', contact_id), ('tags', 'ilike', 'waybill')]",
-                                  help='Select an endpoint to find the Waybill.', required=True)
+    contact = fields.Many2one('res.partner', string='Contact', required=True, help='Select a contact from the Contacts module.')
+    endpoint = fields.Many2one('q_endpoint_catalog.q_endpoint', string='Endpoint',
+                               domain="[('contact_id', '=', contact), ('tags', 'ilike', 'waybill')]",
+                               required=True, help='Select an endpoint to find the Waybill.')
