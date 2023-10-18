0a72fee3e4ceba4afa7ce46e7f1ca3953a29db91
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-18 11:36:56 -0700

    assets

--- tms_customer_waybill/__manifest__.py
+++ tms_customer_waybill/__manifest__.py
@@ -15,7 +15,7 @@
         'views/menus.xml',
     ],
     'assets': {
-        'tms_customer_waybill.assets': [
+        'web.assets_common': [
             'tms_customer_waybill/static/src/xml/*.xml',
             'tms_customer_waybill/static/src/js/*.js',
             'tms_customer_waybill/static/src/css/dist/*.css',  # tailwind generated
