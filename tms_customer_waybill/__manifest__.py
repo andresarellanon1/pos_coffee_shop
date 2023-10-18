f86e6152922fdd01a3ffd26ca5fc9d1d1db3b210
Jes Ale <jes@jes.ale>
2023-10-17 18:56:36 -0700

    views manifest

--- tms_customer_waybill/__manifest__.py
+++ tms_customer_waybill/__manifest__.py
@@ -10,9 +10,9 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+        'views/tms_customer_waybill_views.xml',
         'views/actions.xml',
         'views/menus.xml',
-        'views/tms_customer_waybill_views.xml',
     ],
     'assets': {
         'customer_tms_waybill.assets': [
