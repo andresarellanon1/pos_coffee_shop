fe86835e8db3262919f99340146965abd0308636
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-18 17:51:59 -0700

    a

--- tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
+++ tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
@@ -1,12 +1,12 @@
 /** @odoo-module **/
 
 import { useState } from '@odoo/owl';
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
+// import { standardFieldProps } from "@web/views/fields/standard_field_props";
 // import { Component } from "@odoo/owl";
-import { Widget } from 'web.core'
+import { AbstractField } from 'web.AbstractField'
 import { registry } from "@web/core/registry";
 
-export class CustomerWaybillWidget extends Widget {
+export class CustomerWaybillWidget extends AbstractField {
     setup() {
         this.state = useState({
             // Define your component's state here
@@ -24,5 +24,5 @@ export class CustomerWaybillWidget extends Widget {
 
 CustomerWaybillWidget.template = 'tms_customer_waybill.CustomerWaybillWidget';
 // CustomerWaybillWidget.props = { ...standardFieldProps.props };
-// CustomerWaybillWidget.supportedFieldTypes = ['json']
+CustomerWaybillWidget.supportedFieldTypes = ['json']
 registry.category('fields').add('customer_waybill_widget', CustomerWaybillWidget);
