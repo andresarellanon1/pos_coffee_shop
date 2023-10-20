6cd559f93351f6515f23770d1cb1909d15643e1a
Jes Ale <jes@jes.ale>
2023-10-19 17:20:40 -0700

    custom

--- tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
+++ tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
@@ -1,26 +1,29 @@
 /** @odoo-module **/
 
-// import { Component } from "@odoo/owl";
-// import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { useState } from '@odoo/owl';
-import AbstractField from 'web.AbstractField'
+// import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { Component } from "@odoo/owl";
+// import { AbstractField } from 'web.AbstractField'
 import { registry } from "@web/core/registry";
 
-
-export var CustomerWaybillWidget = AbstractField.extend({
-    supportedFieldTypes: ['json', 'text', 'char'],
-    template: 'tms_customer_waybill.CustomerWaybillWidget',
-    start: function() {
-
-    },
-    _renderEdit: function() {
-        if (this.record.data.enable_bitmap) {
-            this.renderCanvasFromHTML();
-        }
-    },
-    _renderReadonly: function() {
-        this._renderEdit();
+export class CustomerWaybillWidget extends Component {
+    setup() {
+        console.warn(this.props);
+        this.state = useState({
+            // Define your component's state here
+        });
     }
-})
+    /**
+     * @param {boolean} newValue
+     */
+    onChange(newValue) {
+        this.props.update(newValue);
+    }
 
+    // Add your component logic here
+}
+
+// CustomerWaybillWidget.props = { ...standardFieldProps.props };
+CustomerWaybillWidget.supportedFieldTypes = ['json']
+CustomerWaybillWidget.template = 'tms_customer_waybill.CustomerWaybillWidget';
 registry.category('fields').add('customer_waybill_widget', CustomerWaybillWidget);
