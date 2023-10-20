73c4d24e06c0940d2fd01c3f0739717506482908
Jes Ale <jes@jes.ale>
2023-10-19 17:07:47 -0700

    A

--- tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
+++ tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
@@ -1,29 +1,26 @@
 /** @odoo-module **/
 
+// import { Component } from "@odoo/owl";
+// import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { useState } from '@odoo/owl';
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
-import { Component } from "@odoo/owl";
-// import { AbstractField } from 'web.AbstractField'
+import { AbstractField } from 'web.AbstractField'
 import { registry } from "@web/core/registry";
 
-export class CustomerWaybillWidget extends Component {
-    setup() {
-        console.warn(this.props);
-        this.state = useState({
-            // Define your component's state here
-        });
-    }
-    /**
-     * @param {boolean} newValue
-     */
-    onChange(newValue) {
-        this.props.update(newValue);
-    }
 
-    // Add your component logic here
-}
+export var CustomerWaybillWidget = AbstractField.extend({
+    supportedFieldTypes: ['json', 'text', 'char'],
+    template: 'tms_customer_waybill.CustomerWaybillWidget',
+    start: function() {
 
-CustomerWaybillWidget.props = { ...standardFieldProps.props };
-CustomerWaybillWidget.supportedFieldTypes = ['json', 'text', 'char']
-CustomerWaybillWidget.template = 'tms_customer_waybill.CustomerWaybillWidget';
+    },
+    _renderEdit: function() {
+        if (this.record.data.enable_bitmap) {
+            this.renderCanvasFromHTML();
+        }
+    },
+    _renderReadonly: function() {
+        this._renderEdit();
+    }
+})
+
 registry.category('fields').add('customer_waybill_widget', CustomerWaybillWidget);
