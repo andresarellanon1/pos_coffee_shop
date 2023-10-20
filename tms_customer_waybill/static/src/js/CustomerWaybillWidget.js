c9361b3bf106b46505c0fa7d321e49caea6934ab
Jes Ale <jes@jes.ale>
2023-10-19 17:24:58 -0700

    a

--- tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
+++ tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
@@ -1,9 +1,8 @@
 /** @odoo-module **/
 
 import { useState } from '@odoo/owl';
-// import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { Component } from "@odoo/owl";
-// import { AbstractField } from 'web.AbstractField'
 import { registry } from "@web/core/registry";
 
 export class CustomerWaybillWidget extends Component {
@@ -23,7 +22,7 @@ export class CustomerWaybillWidget extends Component {
     // Add your component logic here
 }
 
-// CustomerWaybillWidget.props = { ...standardFieldProps.props };
+CustomerWaybillWidget.props = { ...standardFieldProps.props };
 CustomerWaybillWidget.supportedFieldTypes = ['json']
 CustomerWaybillWidget.template = 'tms_customer_waybill.CustomerWaybillWidget';
 registry.category('fields').add('customer_waybill_widget', CustomerWaybillWidget);
