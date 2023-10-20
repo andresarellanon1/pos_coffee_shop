15d37aeaf46b91a0769bcbec6d8a2c9e07b12291
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-20 14:35:56 -0700

    terminator

--- tms_customer_waybill/wizards/tms_customer_waybill.py
+++ tms_customer_waybill/wizards/tms_customer_waybill.py
@@ -20,22 +20,8 @@ class CustomerWaybillWizard(models.TransientModel):
             custom_attributes = []
             json_response = self.env['q_endpoint_catalog.q_endpoint'].send_request(self.endpoint.id, custom_headers=custom_headers, custom_attributes=custom_attributes)
             try:
-                json_response = json.loads(json_response)
+                response = json.loads(json_response)
+                if isinstance(response, list):
+                    self.remote_waybills = json_response
             except json.JSONDecodeError:
                 raise ValueError("Failed to decode JSON response")
-            if not isinstance(json_response, list):
-                raise ValueError("The JSON response is not a list.")
-            self.remote_waybills = json.dumps(json_response)
-
-    def re_compute_remote_waybills(self):
-        if self.contact and self.endpoint:
-            custom_headers = []
-            custom_attributes = []
-            json_response = self.env['q_endpoint_catalog.q_endpoint'].send_request(self.endpoint.id, custom_headers=custom_headers, custom_attributes=custom_attributes)
-            try:
-                json_response = json.loads(json_response)
-            except json.JSONDecodeError:
-                raise ValueError("Failed to decode JSON response")
-            if not isinstance(json_response, list):
-                raise ValueError("The JSON response is not a list.")
-            self.remote_waybills = json.dumps(json_response)
