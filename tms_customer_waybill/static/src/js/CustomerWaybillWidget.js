6a6c9d9a3ebbac78de4077497eeea182eade14dd
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-20 17:35:00 -0700

    aaa

--- tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
+++ tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
@@ -3,7 +3,8 @@
 // import { useState } from '@odoo/owl';
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { RayderWidget } from "@tms_customer_waybill/js/RyderWidget"
+import { RayderWidget } from "@tms_customer_waybill/js/RayderWidget"
+import { DefaultWidget } from "@tms_customer_waybill/js/RayderWidget"
 
 export class CustomerWaybillWidget extends Component {
     setup() {
@@ -18,5 +19,5 @@ export class CustomerWaybillWidget extends Component {
 
 CustomerWaybillWidget.supportedFieldTypes = ['json']
 CustomerWaybillWidget.template = 'tms_customer_waybill.CustomerWaybillWidget';
-// CustomerWaybillWidget.components = { RayderWidget };
+CustomerWaybillWidget.components = { RayderWidget, DefaultWidget };
 registry.category('fields').add('customer_waybill_widget', CustomerWaybillWidget);
