d9f1be3d6a1731e29bec184a39355a7258812e53
Quadro Soluciones <quadro@q-dev.quadro.com>
2023-10-23 12:01:20 -0700

    aa

--- tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
+++ tms_customer_waybill/static/src/js/CustomerWaybillWidget.js
@@ -77,5 +77,5 @@ export class CustomerWaybillWidget extends Component {
 
 CustomerWaybillWidget.supportedFieldTypes = ['json']
 CustomerWaybillWidget.template = 'tms_customer_waybill.CustomerWaybillWidget';
-CustomerWaybillWidget.components = {};
+// CustomerWaybillWidget.components = {};
 registry.category('fields').add('customer_waybill_widget', CustomerWaybillWidget);
